963104aae57f8589fc69397be2837122147cc808
Jonathan Wetmore <jhcwetmore@phiretech.com>
2020-01-07 15:31:00 -0600

    first commit to push to GitHub

---
+++ destory-the-world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  destory-the-world
+//
+//  Created by Jonathan Wetmore on 1/7/20.
+//  Copyright © 2020 Jonathan Wetmore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ destory-the-world/MyClass.swift
@@ -0,0 +1,13 @@
+//
+//  MyClass.swift
+//  destory-the-world
+//
+//  Created by Jonathan Wetmore on 1/7/20.
+//  Copyright © 2020 Jonathan Wetmore. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+}
